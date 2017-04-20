c27e881c93338c2c53676419f260a31ad7dbadc5
Shiyanov Vadim <vadimsh853@gmail.com>
2017-04-20 17:01:58 +0300

    deleted virus template from std.h

--- 4 sem/include/std.h
+++ 4 sem/include/std.h
@@ -6,10 +6,3 @@
 
 #define BAD_IN -1
 #define FAIL -2
-
-/*
-template<class T> std::ostream& operator<<(std::ostream& s, T& a)
-{
-	return a.put(s);
-}	// use of overloaded operator '<<' is ambiguous (with operand types 'ostream' (aka 'basic_ostream<char>') and 'const char [59]')
-*/

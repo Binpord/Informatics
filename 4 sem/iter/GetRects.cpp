6aef0e8606895be5a4148584e507bd755755d87e
Shiyanov Vadim <vadimsh853@gmail.com>
2017-04-27 11:03:57 +0300

    Tricked myself with some facts such as we find not topLeft in GetRects but bottom left and wxRects were bad.

--- 4 sem/iter/GetRects.cpp
+++ 4 sem/iter/GetRects.cpp
@@ -3,14 +3,15 @@
 #include <wx/string.h>		// for wxString
 #include <wx/image.h>		// for wxImage
 #include <wx/gdicmn.h>		// for wxRect and wxPoint
+#include <wx/colour.h>		// for wxColour
 #include "image.h"		// for Image
 
 #ifndef __DEBUG
 	#define NDEBUG		// for assert to stop
 #endif
 
-void GetRects(const wxImage&, const wxRect&, CList<Image>*);
-wxRect* AddRect(const wxImage&, const wxPoint&, CList<Image>*);
+void GetRects(const wxImage&, const wxRect&, CList<Image>*, const wxColour&);
+wxRect* AddRect(const wxImage&, const wxPoint&, CList<Image>*, const wxColour&);
 
 int main()
 {
@@ -22,24 +23,22 @@ int main()
 
 	// Finding rects in the image. Assuming that they are not intercepting and neither located one under the other.
 	CList<Image> storage;
-	GetRects(in_image, wxRect(0, 0, in_image.GetWidth(), in_image.GetHeight()), &storage);
+	// TODO: if will have time - make histogramm of colors and use output to choose background. Don't use constant values!!!
+	wxColour Background(255, 255, 255);
+	GetRects(in_image, wxRect(0, 0, in_image.GetWidth(), in_image.GetHeight()), &storagem, Background);
 	// TODO: handler + rects usage
 	
 	return 0;
 }
 
 // start_x, start_y, Width and Height defining of the image part the function will scan
-void GetRects(const wxImage& image, const wxRect& part, CList<Image>* storage)
+void GetRects(const wxImage& image, const wxRect& part, CList<Image>* storage, const wxColour& Background)
 {
 	wxRect* AddStatus;
 	int pWidth = part.GetWidth();
 	int pHeight = part.GetHeight();
 	int part_left = part.GetLeft();
 	int part_top = part.GetTop();
-	// TODO: if will have time - make histogramm of colors and use output to choose background. Don't use constant values!!!
-	int BackgroundRed = 255;
-	int BackgroundGreen = 255;
-	int BackgroundBlue = 255;
 
 	// horizontal slice
 	for(int x = part_left; x < pWidth; x++)
@@ -47,10 +46,10 @@ void GetRects(const wxImage& image, const wxRect& part, CList<Image>* storage)
 		// vertical slice
 		for(int y = part_top; y < pHeight; y++)
 		{
-			if(image.GetRed(x, y) != BackgroundRed || image.GetGreen(x, y) != BackgroundGreen || image.GetBlue != BackgroundBlue)
+			if(image.GetRed(x, y) != Background.Red() || image.GetGreen(x, y) != Background.Green() || image.GetBlue != Background.Blue())
 			{
 				wxPoint(x, y);
-				AddStatus = AddRect(image, wxPoint(x, y), storage);
+				AddStatus = AddRect(image, wxPoint(x, y), storage, Background);
 #ifdef __DEBUG
 				if(AddStatus == NULL)
 				{
@@ -59,18 +58,23 @@ void GetRects(const wxImage& image, const wxRect& part, CList<Image>* storage)
 				}
 #endif
 				// Check, whether was rects above the one we added.
-				GetRects(image, wxRect(x, y, AddStatus->GetWidth(), AddStatus->GetHeight()), storage);
+				// Using wxRect(const wxPoint& topLeft, const wxPoint& bottomRight)
+				GetRects(image, wxRect(wxPoint(x, part.GetTop()), wxPoint(x + AddStatus->GetWidth(), y + AddStatus->GetHeight())), storage, Background);
 				// Check, whether was rects beneath the one we added.
-				y += AddStatus.GetHeight();
-				GetRects(image, wxRect(x, y, AddStatus->GetWidth(), AddStatus->GetHeight()), storage);
+				GetRects(image, wxRect(wxPoint(x, y), wxPoint(x + AddStatus->GetWidth(), part.GetBottom())), storage, Background);
 				x += AddStatus.GetWidth();
 				// The recursion will stop when there will be no rects in the part.
-				// TODO: handle lines with width.
 			}
 		}
 	}
 }
 
-wxRect* AddRect(const wxImage& image, const wxPoint& topLeft, CList<Image>* storage) 
+wxRect* AddRect(const wxImage& image, const wxPoint& bottomLeft, CList<Image>* storage, const wxColour& Background) 
 {
+	// TODO: handle lines with width.
+	// Supposing that all lines have same width.
+	wxRect *Res = NULL;
+	int Height = 0, Width = 0;
+
+	for(; image.GetRed(x, y)
 }

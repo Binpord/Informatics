1610ce5cf4c7ec67786b6a37314577b2139af5fa
Shiyanov Vadim <vadimsh853@gmail.com>
2017-05-18 18:28:01 +0300

    Added .h and .cpp for recognition. Actually, it just performs parsing of input image and sends it to opencv svm.

---
+++ 4 sem/Project/handwritten-recognition.cpp
@@ -0,0 +1,149 @@
+#include "handwritten-recognition.h"
+
+wxString GetExpression(wxBitmap* input)
+{
+	wxImage im_input = input->ConvertToImage();
+	std::vector<cv::Mat>* sumbs = DivideSumbols(im_input);
+	wxString result;
+
+	int expr_size = sumbs->size();
+	for(int i = 0; i < expr_size; i++)
+	{
+		result << PerformRecognition((*sumbs)[i]);
+	}
+
+	return result;
+}
+
+std::vector<cv::Mat>* DivideSumbols(const wxImage& image)
+{
+	int width = image.GetWidth();
+	int count = 0;
+	std::vector<cv::Mat>* result = new std::vector<cv::Mat>;
+	while(count < width)
+	{
+		count += CountWhiteCols(image, count);
+		if(count == width)
+			break;
+		
+		int left = count;
+		for(count; CountWhiteCols(image, count) == 0 && count < width; count++);
+
+		int right = count;
+
+		wxRect sumbol_rect = CountTopAndBot(image, left, right);
+		wxImage sumbol = image.GetSubImage(sumbol_rect);
+#ifdef __DEBUG
+		wxInitAllImageHandlers();
+		sumbol.SaveFile("test1.png", wxBITMAP_TYPE_PNG);
+#endif
+		sumbol.Rescale(28, 28);
+#ifdef __DEBUG
+		sumbol.SaveFile("test2.png", wxBITMAP_TYPE_PNG);
+#endif
+		SwitchColors(&sumbol);
+#ifdef __DEBUG
+		sumbol.SaveFile("test3.png", wxBITMAP_TYPE_PNG);
+#endif
+		cv::Mat cv_sumbol;
+		Wx2CvImage(sumbol, cv_sumbol);
+		result->push_back(rgb2gray(cv_sumbol));
+	}
+
+	return result;
+}
+
+int CountWhiteCols(const wxImage& image, int start)
+{
+	int width = image.GetWidth(), height = image.GetHeight();
+	int x = start;
+	for(; x < width; x++)
+	{
+		for(int y = 0; y < height; y++)
+		{
+			if(image.GetRed(x, y) != 255 || image.GetGreen(x, y) != 255 || image.GetBlue(x, y) != 255)
+				goto found_non_white;
+		}
+	}
+found_non_white:
+
+	return x - start;
+}
+
+wxRect CountTopAndBot(const wxImage& image, int left, int right)
+{
+	int height = image.GetHeight();
+	int top = 0, bottom = height;
+
+	for(int y = 0; y < height; y++)
+	{
+		for(int x = left; x < right; x++)
+		{
+			if(image.GetRed(x, y) != 255 || image.GetGreen(x, y) != 255 || image.GetBlue(x, y) != 255)
+			{
+				top = y;
+				goto bottom;
+			}
+		}
+	}
+
+bottom:
+	for(int y = height - 1; y >= 0; y--)
+	{
+		for(int x = left; x < right; x++)
+		{
+			if(image.GetRed(x, y) != 255 || image.GetGreen(x, y) != 255 || image.GetBlue(x, y) != 255)
+			{
+				bottom = y;
+				goto end;
+			}
+		}
+	}
+end:
+
+	return wxRect(wxPoint(left, top), wxPoint(right, bottom));
+}
+
+wxString PerformRecognition(const cv::Mat& sumbol)
+{
+#ifdef __DEBUG
+	cv::imwrite("test.jpg", sumbol);
+#endif
+	cv::Ptr<cv::ml::SVM> svm = cv::ml::StatModel::load<cv::ml::SVM>("classifier.yml");
+
+	cv::Mat testMat = sumbol.clone().reshape(1,1);
+	testMat.convertTo(testMat, CV_32F);
+	
+	int predicted = 0;
+
+	try
+	{
+		predicted = svm->predict(testMat);
+	}
+	catch(cv::Exception ex)
+	{}
+
+	wxString res;
+	res << predicted;
+	return res;
+}
+
+void SwitchColors(wxImage* image)
+{
+	wxColor black(0, 0, 0), white(255, 255, 255);
+	int width = image->GetWidth(), height = image->GetHeight();
+	for(int x = 0; x < width; x++)
+	{
+		for(int y = 0; y < height; y++)
+		{
+			if(image->GetRed(x, y) != white.Red() || image->GetGreen(x, y) != white.Green() || image->GetBlue(x, y) != white.Blue())
+			{
+				image->SetRGB(x, y, white.Red(), white.Green(), white.Blue());
+			}
+			else
+			{
+				image->SetRGB(x, y, black.Red(), black.Green(), black.Blue());
+			}
+		}
+	}
+}
---
+++ 4 sem/Project/handwritten-recognition.h
@@ -0,0 +1,12 @@
+#include <wx/wx.h>
+#include <wx/bitmap.h>
+#include <wx/gdicmn.h>
+#include "WxConvertCv.h"
+
+std::vector<cv::Mat>* DivideSumbols(const wxImage&);
+wxString GetExpression(wxBitmap* input);
+int Evaluate(const wxString& expr);
+wxString PerformRecognition(const cv::Mat&);
+int CountWhiteCols(const wxImage&, int);
+wxRect CountTopAndBot(const wxImage&, int, int);
+void SwitchColors(wxImage*);
